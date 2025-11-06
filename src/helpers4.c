6fb4b720cf261b9190e56a06bf2b034eda234a01
pokiecake <24tnguyen25@gmail.com>
2025-11-06 07:10:47 +0000

    initial code for malloc

--- src/helpers4.c
+++ src/helpers4.c
@@ -2,3 +2,48 @@
 #include "debug.h"
 
 /* Helper function definitions go here */
+void initialize_prologue(ics_footer * prologue) {
+	prologue->block_size = 1; //0 size, 1 allocated
+	prologue->fid = FOOTER_MAGIC;
+	prologue->requested_size = 0
+}
+
+void initialize_epilogue(ics_header * epilogue) {
+	epilogue->block_size = 1; //0 size, 1 allocated
+	epilogue->hid = HEADER_MAGIC;
+}
+
+void set_header(ics_header * header, uint64_t size, uint64_t is_allocated) {
+	header->block_size = size + is_allocated; //pack the allocated bit into the size bits
+	header->hid = HEADER_MAGIC;
+}
+
+void set_footer(ics_footer * footer, uint64_t size, uint64_t requested_size, uint64_t is_allocated) {
+	footer->block_size = size + is_allocated;//pack the allocated bit into the size bits
+	footer->requested_size = requested_size;
+	footer->fid = FOOTER_MAGIC;
+}
+
+void set_free_header(ics_free_header * free_header, uint64_t size, uint64_t is_allocated, ics_free_header * next, ics_free_header * prev) {
+	set_header(free_header->header, size ,is_allocated);
+	free_header->next = next;
+	free_header->prev = prev;
+}
+
+char * allocate_block(uint64_t block_size, uint64_t requested_size, char * cur_addr) {
+	ics_free_header * free_header = cur_addr - 8;
+	if (free_header->header->size >= block_size) {
+		//if there is no fragmentation, allocate just enough and split the free block
+		uint64_t new_free_block_size = free_header->header->size - block_size;
+		ics_free_header * new_block_next = free_header->next;
+		ics_free_header * new_block_prev = free_header->prev;
+		
+		set_header(header, block_size, 1);
+		//footer will be 8 bytes away from the end of the block
+		set_footer(header + block_size - 8);
+
+		set_free_header(header + block_size, new_free_block_size, 0, new_block_next, new_block_prev);
+		// !!set linked list head to the new block if needed
+		// !!set prev and next lists to point to new address
+	}
+}
--- src/icsmm.c
+++ src/icsmm.c
@@ -4,9 +4,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int BLOCK_MULTIPLE_SIZE = 16;
+const int MAX_PAGES = 6;
+char * mem_ptr = NULL;
+char * brk_ptr = NULL;
+const int PAGE_SIZE = 4096;
+const int PROLOGUE_SIZE = 8;
+const int EPILOGUE_SIZE = 8;
+const int PRO_EPI_SIZE = 16;
 
 void *ics_malloc(size_t size) { 
-    return NULL;
+	if (mem_ptr == NULL) { //very first call to malloc
+		// !!check for too many pages
+		mem_ptr = ics_inc_brk(); 
+		brk_ptr = ics_get_brk();
+		ics_footer * prologue = mem_ptr; 
+		ics_free_header * start_header = mem_ptr + PROLOGUE_SIZE;
+		ics_header * epilogue = brk_ptr - EPILOGUE_SIZE;
+		
+		initialize_prologue(prologue);
+		initialize_epilogue(epilogue);
+		//set the header of the free block with the size = current page size - size of prologue and epilogue
+		//set it as the start of the linked list (in the buckets??)
+		set_free_header(start_header, PAGE_SIZE - PRO_EPI_SIZE, 0, NULL, NULL);
+		// !!set footer too
+	}
+	
+	//first time: allocate space (huge chunk of memory)
+	//-install prologue and epilogue (8 byters)
+	//-install header and footer
+	
+	//calculate minimum block size
+	size_t remainder = size % BLOCK_MULTIPLE_SIZE;
+	// if size is a multiple of 16, payload is the size. Else, move on to the next multiple of 16;
+	size_t payload_size = (remainder * BLOCK_MULTIPLE_SIZE == size ? size : (remainder + 1) * BLOCK_MULTIPLE_SIZE;
+	size_t block_size = payload_size + PRO_EPI_SIZE;
+	//find bucket to hold data
+	//-if no splinter, split free block into allocated space and free chunck, update headers
+	//--put free chunk into a fitting list LIFO
+	//-if splinter, take the whole chunk and updat headers
+	
+	
+    return NULL;//return address of payload
 }
 
 
